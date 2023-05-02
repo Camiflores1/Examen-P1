7bacda79c83e8990877b8b6a05ffc0a254f17318
Camiflores1 <monse.flores0108@gmail.com>
2023-05-02 12:42:07 -0500

    Escojo que estructura de repetición voy para hallar el valor absoluto de números negativos.

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,13 +1,17 @@
 //Examen Práctico P1
 //Realice un programa para hallar cuántos números se debieron haber leído (ingresados por el usuario) para que la suma de los números negativos dé en valor absoluto mayor que 1200.
-
-
-int main 
+ 
 #include <stdio.h>
 
 int main() {
     //inicializamos las variables
    int num, sum = 0, count = 0;
-    
+    //Defino que estructuras de repetición voy a usar
    do {
-  
+       // se pide al usuario que ingrese un número
+      printf("Ingrese un número: ");
+      scanf("%d", &num);
+      if (num < 0) {
+          // Se suma el valor absoluto de los números negativos (suma=suma-num)
+         sum -= num; 
+      }
