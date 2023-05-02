31d0e4aac6870f915218d2805cf89548e9b6a68c
Camiflores1 <monse.flores0108@gmail.com>
2023-05-02 12:30:36 -0500

    Parte 1, lo que hice fue definir las variables que voy a utlizar para crear el programa.

---
+++ ejercicio1.c
@@ -0,0 +1,13 @@
+//Examen Práctico P1
+//Realice un programa para hallar cuántos números se debieron haber leído (ingresados por el usuario) para que la suma de los números negativos dé en valor absoluto mayor que 1200.
+
+
+int main 
+#include <stdio.h>
+
+int main() {
+    //inicializamos las variables
+   int num, sum = 0, count = 0;
+    
+   do {
+  
