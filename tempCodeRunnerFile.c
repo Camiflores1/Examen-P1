2470c2a843da17ab841956ced77887465476ec39
Camiflores1 <monse.flores0108@gmail.com>
2023-05-02 12:52:11 -0500

    Parte 3: al ingresar los número el programa ya determina los números ingresados para que la suma de en valor absoluto.

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,6 +1,3 @@
-//Examen Práctico P1
-//Realice un programa para hallar cuántos números se debieron haber leído (ingresados por el usuario) para que la suma de los números negativos dé en valor absoluto mayor que 1200.
- 
 #include <stdio.h>
 
 int main() {
